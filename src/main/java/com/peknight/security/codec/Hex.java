fce32ef74c47370c5a1ad2fd9844b9266ebe59f2
peknight <peknight@qq.com>
2017-07-14 17:38:06 +0800

    Add Class Hex byte[] to hex char[]

---
+++ src/main/java/com/peknight/security/codec/Hex.java
@@ -0,0 +1,51 @@
+/**
+ * MIT License
+ * <p>
+ * Copyright (c) 2017-2027 PeKnight(JKpeknight@gmail.com)
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.peknight.security.codec;
+
+/**
+ * 十六进制相关操作
+ *
+ * @author PeKnight
+ *
+ * Created by PeKnight on 2017/7/14.
+ */
+public class Hex {
+    private static final char[] DIGITS = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+    /**
+     * 二进制数组转为16进制字符数组
+     *
+     * @param data
+     * @return
+     */
+    public static char[] encode(byte[] data) {
+        int length = data.length;
+        char[] result = new char[length * 2];
+        for (int i = 0, index = 0; i < length; i++) {
+            result[index++] = DIGITS[(240 & data[i]) >>> 4];
+            result[index++] = DIGITS[15 & data[i]];
+        }
+        return result;
+    }
+}
