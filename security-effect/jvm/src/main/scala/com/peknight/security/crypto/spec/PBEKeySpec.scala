a61290624195ee4dda8aef3ef7d56d1cc575a442
peknight <JKpeknight@gmail.com>
2024-06-27 18:02:17 +0800

    update

--- security-effect/jvm/src/main/scala/com/peknight/security/crypto/spec/PBEKeySpec.scala
+++ security-effect/jvm/src/main/scala/com/peknight/security/crypto/spec/PBEKeySpec.scala
@@ -1,14 +1,13 @@
 package com.peknight.security.crypto.spec
 
-import cats.effect.Sync
 import scodec.bits.ByteVector
 
 import javax.crypto.spec.PBEKeySpec as JPBEKeySpec
 
 object PBEKeySpec:
-  def apply[F[_]: Sync](password: String): F[JPBEKeySpec] = Sync[F].delay(JPBEKeySpec(password.toCharArray))
-  def apply[F[_]: Sync](password: String, salt: ByteVector, iterationCount: Int, keyLength: Int): F[JPBEKeySpec] =
-    Sync[F].delay(JPBEKeySpec(password.toCharArray, salt.toArray, iterationCount, keyLength))
-  def apply[F[_]: Sync](password: String, salt: ByteVector, iterationCount: Int): F[JPBEKeySpec] =
-    Sync[F].delay(JPBEKeySpec(password.toCharArray, salt.toArray, iterationCount))
+  def apply(password: String): JPBEKeySpec = JPBEKeySpec(password.toCharArray)
+  def apply(password: String, salt: ByteVector, iterationCount: Int, keyLength: Int): JPBEKeySpec =
+    JPBEKeySpec(password.toCharArray, salt.toArray, iterationCount, keyLength)
+  def apply(password: String, salt: ByteVector, iterationCount: Int): JPBEKeySpec =
+    JPBEKeySpec(password.toCharArray, salt.toArray, iterationCount)
 end PBEKeySpec
--- security-effect/jvm/src/main/scala/com/peknight/security/crypto/spec/PBEParameterSpec.scala
+++ security-effect/jvm/src/main/scala/com/peknight/security/crypto/spec/PBEParameterSpec.scala
@@ -1,14 +1,13 @@
 package com.peknight.security.crypto.spec
 
-import cats.effect.Sync
 import scodec.bits.ByteVector
 
 import java.security.spec.AlgorithmParameterSpec
 import javax.crypto.spec.PBEParameterSpec as JPBEParameterSpec
 
 object PBEParameterSpec:
-  def apply[F[_]: Sync](salt: ByteVector, iterationCount: Int): F[JPBEParameterSpec] =
-    Sync[F].delay(JPBEParameterSpec(salt.toArray, iterationCount))
-  def apply[F[_]: Sync](salt: ByteVector, iterationCount: Int, paramSpec: AlgorithmParameterSpec): F[JPBEParameterSpec] =
-    Sync[F].delay(JPBEParameterSpec(salt.toArray, iterationCount, paramSpec))
+  def apply(salt: ByteVector, iterationCount: Int): JPBEParameterSpec =
+    JPBEParameterSpec(salt.toArray, iterationCount)
+  def apply(salt: ByteVector, iterationCount: Int, paramSpec: AlgorithmParameterSpec): JPBEParameterSpec =
+    JPBEParameterSpec(salt.toArray, iterationCount, paramSpec)
 end PBEParameterSpec
