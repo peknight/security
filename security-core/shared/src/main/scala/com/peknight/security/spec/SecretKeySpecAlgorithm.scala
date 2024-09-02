7c5e348af6a4c8f59bf26750766abf2774860feb
peknight <JKpeknight@gmail.com>
2024-09-02 23:29:08 +0800

    update

--- security-core/shared/src/main/scala/com/peknight/security/spec/SecretKeySpecAlgorithm.scala
+++ security-core/shared/src/main/scala/com/peknight/security/spec/SecretKeySpecAlgorithm.scala
@@ -2,4 +2,4 @@ package com.peknight.security.spec
 
 import com.peknight.security.algorithm.Algorithm
 
-trait SecretKeySpecAlgorithm extends Algorithm
+trait SecretKeySpecAlgorithm extends Algorithm with SecretKeySpecAlgorithmPlatform
