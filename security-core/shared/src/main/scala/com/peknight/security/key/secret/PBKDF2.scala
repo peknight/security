9abbeb8728e288f6feb7531d24b4e511d8a260e9
peknight <JKpeknight@gmail.com>
2024-11-06 20:42:16 +0800

    update

--- security-core/shared/src/main/scala/com/peknight/security/key/secret/PBKDF2.scala
+++ security-core/shared/src/main/scala/com/peknight/security/key/secret/PBKDF2.scala
@@ -1,8 +1,11 @@
 package com.peknight.security.key.secret
 
 import com.peknight.security.random.PRF
+import com.peknight.security.spec.SecretKeySpecAlgorithm
 
-trait PBKDF2
+trait PBKDF2 extends SecretKeySpecAlgorithm:
+  def algorithm: String = "PBKDF2"
+end PBKDF2
 object PBKDF2:
   def withPRF(prf: PRF): PBKDF2WithPRF = PBKDF2WithPRF(prf)
 end PBKDF2
