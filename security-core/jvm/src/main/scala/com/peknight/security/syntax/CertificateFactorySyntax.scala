3d8161339ac92fa9e2d860855bef0ede89d29eb8
peknight <JKpeknight@gmail.com>
2025-03-30 10:57:56 +0800

    update

--- security-core/jvm/src/main/scala/com/peknight/security/syntax/CertificateFactorySyntax.scala
+++ security-core/jvm/src/main/scala/com/peknight/security/syntax/CertificateFactorySyntax.scala
@@ -1,6 +1,6 @@
 package com.peknight.security.syntax
 
-import cats.effect.kernel.Sync
+import cats.effect.Sync
 
 import java.io.InputStream
 import java.security.cert.{Certificate, CertificateFactory}
@@ -11,7 +11,7 @@ trait CertificateFactorySyntax:
     def generateCertificateF[F[_]: Sync](inStream: InputStream): F[Certificate] =
       Sync[F].blocking(certificateFactory.generateCertificate(inStream))
 
-    def generateCertificatesF[F[_] : Sync](inStream: InputStream): F[List[Certificate]] =
+    def generateCertificatesF[F[_]: Sync](inStream: InputStream): F[List[Certificate]] =
       Sync[F].blocking(certificateFactory.generateCertificates(inStream).asScala.toList)
   end extension
 end CertificateFactorySyntax
