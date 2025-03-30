d1eea1fb1277a0a7f5bda8d1e08cf800315e39b1
peknight <JKpeknight@gmail.com>
2025-03-30 18:32:55 +0800

    update

--- security-bcpkix/jvm/src/main/scala/com/peknight/security/bouncycastle/openssl/PEMParser.scala
+++ security-bcpkix/jvm/src/main/scala/com/peknight/security/bouncycastle/openssl/PEMParser.scala
@@ -1,28 +1,16 @@
 package com.peknight.security.bouncycastle.openssl
 
-import cats.effect.{Resource, Sync}
-import cats.syntax.applicativeError.*
+import cats.effect.Sync
 import fs2.io.file.Path
 import org.bouncycastle.openssl.PEMParser as JPEMParser
 
 import java.io.{FileReader, Reader as JReader}
 
 object PEMParser:
+  def apply[F[_]: Sync](path: Path): F[JPEMParser] =
+    Sync[F].blocking(new JPEMParser(new FileReader(path.toNioPath.toFile)))
 
-  def apply[F[_] : Sync](path: Path): Resource[F, JPEMParser] =
-    Resource.fromAutoCloseable[F, JPEMParser](Sync[F].blocking(new JPEMParser(new FileReader(path.toNioPath.toFile))))
-
-  def handleReleaseErrorWith[F[_] : Sync](path: Path)(f: Throwable => F[Unit]): Resource[F, JPEMParser] =
-    Resource.make[F, JPEMParser](Sync[F].blocking(new JPEMParser(new FileReader(path.toNioPath.toFile))))(closeable =>
-      Sync[F].blocking(closeable.close()).handleErrorWith(f)
-    )
-
-  def apply[F[_]: Sync](reader: JReader): Resource[F, JPEMParser] =
-    Resource.fromAutoCloseable[F, JPEMParser](Sync[F].blocking(new JPEMParser(reader)))
-
-  def handleReleaseErrorWith[F[_]: Sync](reader: JReader)(f: Throwable => F[Unit]): Resource[F, JPEMParser] =
-    Resource.make[F, JPEMParser](Sync[F].blocking(new JPEMParser(reader)))(
-      closeable => Sync[F].blocking(closeable.close()).handleErrorWith(f)
-    )
+  def apply[F[_]: Sync](reader: JReader): F[JPEMParser] =
+    Sync[F].blocking(new JPEMParser(reader))
 end PEMParser
 
--- security-bcpkix/jvm/src/main/scala/com/peknight/security/bouncycastle/openssl/jcajce/JcaPEMWriter.scala
+++ security-bcpkix/jvm/src/main/scala/com/peknight/security/bouncycastle/openssl/jcajce/JcaPEMWriter.scala
@@ -1,29 +1,15 @@
 package com.peknight.security.bouncycastle.openssl.jcajce
 
-import cats.effect.{Resource, Sync}
-import cats.syntax.applicativeError.*
+import cats.effect.Sync
 import fs2.io.file.Path
 import org.bouncycastle.openssl.jcajce.JcaPEMWriter as JJcaPEMWriter
 
 import java.io.{FileWriter, Writer as JWriter}
 
 object JcaPEMWriter:
-  def apply[F[_] : Sync](path: Path): Resource[F, JJcaPEMWriter] =
-    Resource.fromAutoCloseable[F, JJcaPEMWriter](
-      Sync[F].blocking(new JJcaPEMWriter(new FileWriter(path.toNioPath.toFile)))
-    )
+  def apply[F[_]: Sync](path: Path): F[JJcaPEMWriter] =
+    Sync[F].blocking(new JJcaPEMWriter(new FileWriter(path.toNioPath.toFile)))
 
-  def handleReleaseErrorWith[F[_] : Sync](path: Path)(f: Throwable => F[Unit])
-  : Resource[F, JJcaPEMWriter] =
-    Resource.make[F, JJcaPEMWriter](Sync[F].blocking(new JJcaPEMWriter(new FileWriter(path.toNioPath.toFile))))(
-      closeable => Sync[F].blocking(closeable.close()).handleErrorWith(f)
-    )
-
-  def apply[F[_]: Sync](writer: JWriter): Resource[F, JJcaPEMWriter] =
-    Resource.fromAutoCloseable[F, JJcaPEMWriter](Sync[F].blocking(new JJcaPEMWriter(writer)))
-
-  def handleReleaseErrorWith[F[_]: Sync](writer: JWriter)(f: Throwable => F[Unit]): Resource[F, JJcaPEMWriter] =
-    Resource.make[F, JJcaPEMWriter](Sync[F].blocking(new JJcaPEMWriter(writer)))(closeable =>
-      Sync[F].blocking(closeable.close()).handleErrorWith(f)
-    )
+  def apply[F[_]: Sync](writer: JWriter): F[JJcaPEMWriter] =
+    Sync[F].blocking(new JJcaPEMWriter(writer))
 end JcaPEMWriter
