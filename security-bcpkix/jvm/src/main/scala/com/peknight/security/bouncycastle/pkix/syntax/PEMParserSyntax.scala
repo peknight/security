ede1e67dcec0d0b2f7392c29c6deb42511ce2dd6
peknight <JKpeknight@gmail.com>
2025-03-31 08:37:28 +0800

    update

--- security-bcpkix/jvm/src/main/scala/com/peknight/security/bouncycastle/pkix/syntax/PEMParserSyntax.scala
+++ security-bcpkix/jvm/src/main/scala/com/peknight/security/bouncycastle/pkix/syntax/PEMParserSyntax.scala
@@ -1,11 +1,18 @@
 package com.peknight.security.bouncycastle.pkix.syntax
 
 import cats.effect.Sync
+import cats.syntax.functor.*
+import com.peknight.error.Error
+import com.peknight.error.syntax.applicativeError.asError
+import com.peknight.validation.std.either.typed
 import org.bouncycastle.openssl.PEMParser
+
+import scala.reflect.ClassTag
 
 trait PEMParserSyntax:
   extension (parser: PEMParser)
-    def readObjectF[F[_]: Sync, O]: F[O] = Sync[F].blocking(parser.readObject().asInstanceOf[O])
+    def readObjectF[F[_]: Sync, O: ClassTag]: F[Either[Error, O]] =
+      Sync[F].blocking(typed[O](parser.readObject())).asError.map(_.flatten)
   end extension
 end PEMParserSyntax
 object PEMParserSyntax extends PEMParserSyntax
