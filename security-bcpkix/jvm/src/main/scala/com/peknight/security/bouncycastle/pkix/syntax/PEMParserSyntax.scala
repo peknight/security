b08c09c26d3c47fefa1278abe1227eca14bd9139
peknight <JKpeknight@gmail.com>
2025-04-06 23:42:12 +0800

    update

--- security-bcpkix/jvm/src/main/scala/com/peknight/security/bouncycastle/pkix/syntax/PEMParserSyntax.scala
+++ security-bcpkix/jvm/src/main/scala/com/peknight/security/bouncycastle/pkix/syntax/PEMParserSyntax.scala
@@ -3,8 +3,6 @@ package com.peknight.security.bouncycastle.pkix.syntax
 import cats.effect.Sync
 import org.bouncycastle.openssl.PEMParser
 
-import scala.reflect.ClassTag
-
 trait PEMParserSyntax:
   extension (parser: PEMParser)
     def readObjectF[F[_]: Sync]: F[Option[AnyRef]] =
