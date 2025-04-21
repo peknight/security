99f561b12c0f48532293659536d1eec57ba2a2c6
peknight <JKpeknight@gmail.com>
2025-04-21 22:29:27 +0800

    update

--- security-bcpkix/jvm/src/main/scala/com/peknight/security/bouncycastle/operator/jcajce/JcaContentSignerBuilder.scala
+++ security-bcpkix/jvm/src/main/scala/com/peknight/security/bouncycastle/operator/jcajce/JcaContentSignerBuilder.scala
@@ -1,9 +1,18 @@
 package com.peknight.security.bouncycastle.operator.jcajce
 
+import com.peknight.security.provider.Provider
 import com.peknight.security.signature.SignatureAlgorithm
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder as JJcaContentSignerBuilder
 
+import java.security.Provider as JProvider
+
 object JcaContentSignerBuilder:
-  def apply(signatureAlgorithm: SignatureAlgorithm): JJcaContentSignerBuilder =
-    new JJcaContentSignerBuilder(signatureAlgorithm.algorithm)
+  def apply(signatureAlgorithm: SignatureAlgorithm, provider: Option[Provider | JProvider] = None)
+  : JJcaContentSignerBuilder =
+    val builder = new JJcaContentSignerBuilder(signatureAlgorithm.algorithm)
+    provider match
+      case Some(provider: Provider) => builder.setProvider(provider.name)
+      case Some(provider: JProvider) => builder.setProvider(provider)
+      case _ => builder
+
 end JcaContentSignerBuilder
