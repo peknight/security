55b58012927bb25b9b268fdea7cc931a15f16c56
peknight <JKpeknight@gmail.com>
2025-04-22 09:00:46 +0800

    update

--- security-core/shared/src/main/scala/com/peknight/security/signature/SignatureAlgorithm.scala
+++ security-core/shared/src/main/scala/com/peknight/security/signature/SignatureAlgorithm.scala
@@ -1,3 +1,9 @@
 package com.peknight.security.signature
 
 trait SignatureAlgorithm extends Signature with SignatureAlgorithmPlatform
+object SignatureAlgorithm:
+  def raw(alg: String): SignatureAlgorithm =
+    new SignatureAlgorithm:
+      def algorithm: String = alg
+  end raw
+end SignatureAlgorithm
